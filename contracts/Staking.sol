51cf4175319da58935f9211d2707716f368ae922
PhantomOz <faniogor@gmail.com>
2024-02-17 02:31:28 +0100

    This commit will add the stake function and Mappings regarding amount and time, including custom errors

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -2,10 +2,23 @@
 pragma solidity ^0.8.19;
 import "./IERC20.sol";
 
+error AMOUNT_CANT_BE_ZERO();
+error INSUFFICIENT_FUNDS();
+
 contract Staking {
     IERC20 token;
 
     constructor(address _token) {
         token = IERC20(_token);
+    }
+
+    function stake(uint256 _amount) external {
+        if (_amount <= 0) {
+            revert AMOUNT_CANT_BE_ZERO();
+        }
+        if (_amount > token.balanceOf(msg.sender)) {
+            revert INSUFFICIENT_FUNDS();
+        }
+        token.transferFrom(msg.sender, address(this), _amount);
     }
 }

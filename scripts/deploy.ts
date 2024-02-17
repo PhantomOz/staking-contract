39c53906c68b84f98f7ae05ff79d23932a537f5b
PhantomOz <faniogor@gmail.com>
2024-02-17 04:11:45 +0100

    This commit will edit deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,21 +1,13 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const ERC20 = "";
+  const staking = await ethers.deployContract("Staking", [ERC20]);
 
-  const lockedAmount = ethers.parseEther("0.001");
-
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
+  await staking.waitForDeployment();
 
   console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+    `deployed to ${staking.target}`
   );
 }
 

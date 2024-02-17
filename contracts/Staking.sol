220ceaf00e5953c4ee4a1299bec3ee8c7932f664
PhantomOz <faniogor@gmail.com>
2024-02-17 02:08:31 +0100

    This commit will add ERC20 interface and initialize it in the constructor

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -1,4 +1,11 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
+import "./IERC20.sol";
 
-contract Staking {}
+contract Staking {
+    IERC20 token;
+
+    constructor(address _token) {
+        token = IERC20(_token);
+    }
+}
